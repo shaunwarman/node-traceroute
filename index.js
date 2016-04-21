4fbd484e0db2a832d680a07f5059fc522e69a190
Shaun Warman <swarman@paypal.com>
2016-04-21 10:11:36 -0500

    Reformat

--- index.js
+++ index.js
@@ -5,34 +5,34 @@ const exec = require('child_process').exec;
 const trace = {};
 
 trace.lookup = (hostname, callback) => {
-	const source = Rx.Observable.create((observer) => {
-		exec('traceroute paypal.com', (error, stdout, stderr) => {
-			observer.onNext(stdout);
-		});
-	});
+    const source = Rx.Observable.create((observer) => {
+        exec('traceroute paypal.com', (error, stdout, stderr) => {
+            observer.onNext(stdout);
+        });
+    });
 
-	source.subscribe((routeInfo) => {
-		console.log(getIp(cleanLines(splitByLine(routeInfo))));
-	});
+    source.subscribe((routeInfo) => {
+        console.log(getIp(cleanLines(splitByLine(routeInfo))));
+    });
 
-	source.dispose();
+    source.dispose();
 
 };
 
 function splitByLine(result) {
-	return result.split('\n');
+    return result.split('\n');
 }
 
 function cleanLines(eachLine) {
-	return eachLine.filter((line) => {
-		return (line.indexOf('*') === -1);
-	});
+    return eachLine.filter((line) => {
+        return (line.indexOf('*') === -1);
+    });
 }
 
 function getIp(cleanLines) {
-	return cleanLines.map((cleanLine) => {
-		return cleanLine.slice(cleanLine.indexOf('(') + 1, cleanLine.indexOf(')'));
-	});
+    return cleanLines.map((cleanLine) => {
+        return cleanLine.slice(cleanLine.indexOf('(') + 1, cleanLine.indexOf(')'));
+    });
 }
 
 module.exports = trace;
